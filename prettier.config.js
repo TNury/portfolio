c31c6924ca279fc60a5627e815ce1ea9d81de067
Yuri Souza <yurdesou@gmail.com>
2023-11-12 20:06:55 -0300

    Add initial configurations for project

---
+++ prettier.config.js
@@ -0,0 +1,35 @@
+/*
+  Docs for configs:
+  https://prettier.io/docs/en/options.html
+*/
+
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  importOrder: [
+    '^react$',
+    '^next',
+    '<THIRD_PARTY_MODULES>',
+    '^clsx$',
+    '^@portfolio/services',
+    '^@portfolio/components',
+    '^@portfolio/actions',
+    '^@portfolio/lib',
+    '^@portfolio/types',
+    '^@portfolio/styles',
+    '^[src/]',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+};
