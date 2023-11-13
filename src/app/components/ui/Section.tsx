5c9d63259a190761639f807188fe7b83c004e661
Yuri Souza <yurdesou@gmail.com>
2023-11-12 22:54:04 -0300

    Refactor Section component to simplify styling

--- src/app/components/ui/Section.tsx
+++ src/app/components/ui/Section.tsx
@@ -3,13 +3,5 @@ import { cn } from '@portfolio/lib/lib';
 type SectionProps = React.HTMLAttributes<HTMLDivElement>;
 
 export const Section: React.FC<SectionProps> = ({ children, className }) => {
-  return (
-    <section
-      className={cn(
-        'relative flex min-h-screen items-center justify-center overflow-hidden px-4 py-28',
-        className
-      )}>
-      {children}
-    </section>
-  );
+  return <section className={cn('px-4 py-28', className)}>{children}</section>;
 };
