0f71ac4a210a90c4d6d1b3d5bac1038901ac63a1
Yuri Souza <yurdesou@gmail.com>
2023-11-12 21:33:01 -0300

    Install and implement geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import '@portfolio/styles/globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { GeistMono } from 'geist/font/mono';
 
 export const metadata: Metadata = {
   title: 'Yuri Souza',
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={GeistMono.className}>{children}</body>
     </html>
   );
 }
