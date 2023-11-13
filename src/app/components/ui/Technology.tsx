2451ebfdc8cddec0f8fb698459b8cc3274208ed7
Yuri Souza <yurdesou@gmail.com>
2023-11-12 22:53:04 -0300

    Create UI component Technology

---
+++ src/app/components/ui/Technology.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import { cn } from '@portfolio/lib/lib';
+
+type TechnologyProps = {
+  name: string;
+  className?: string;
+};
+
+export const Technology: React.FC<TechnologyProps> = ({ name, className }) => {
+  return (
+    <div className={cn('flex gap-2', className)}>
+      <Image
+        alt='chevron_right'
+        src='assets/media/svg/chevron_right.svg'
+        width={24}
+        height={24}
+      />
+      <p className='text-base text-text-1'>{name}</p>
+    </div>
+  );
+};
