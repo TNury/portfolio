282c282814a67e99320720429fe27debcb8fb9da
Yuri Souza <yurdesou@gmail.com>
2023-11-12 22:54:19 -0300

    Create About section and implement it on homepage

---
+++ src/app/components/sections/About.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+
+import { Section } from '@portfolio/components/ui/Section';
+import { Technology } from '@portfolio/components/ui/Technology';
+
+export const About = () => {
+  return (
+    <Section className='flex flex-col gap-20'>
+      <h2 className='text-4xl font-bold text-text-2'>Sobre mim.</h2>
+      <div className='flex flex-col gap-16'>
+        <div className='flex flex-col gap-8'>
+          <div className='flex flex-col gap-4'>
+            <p className='text-lg text-text-1'>
+              Lorem ipsum dolor sit amet consectetur. Vulputate augue at
+              ultrices mauris convallis interdum tincidunt. Sed eu malesuada
+              pellentesque ut.
+            </p>
+            <p className='text-lg text-text-1'>
+              Lorem ipsum dolor sit amet consectetur. Vulputate augue at
+              ultrices mauris convallis interdum tincidunt. Sed eu malesuada
+              pellentesque ut. Ac vestibulum eu enim mi quis sagittis egestas.
+            </p>
+            <p className='text-lg text-text-1'>
+              Gravida quis arcu metus lacus tristique amet augue.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3 className='text-xl-b font-bold text-text-2'>Web</h3>
+            <div className='flex flex-col gap-1'>
+              <Technology name='JavaScript (ES6+)' />
+              <Technology name='TypeScript' />
+              <Technology name='React' />
+              <Technology name='Next.JS (13 & 14)' />
+              <Technology name='MUI V4 & V5' />
+              <Technology name='TailwindCSS' />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3 className='text-xl-b font-bold text-text-2'>Mobile</h3>
+            <div className='flex flex-col gap-1'>
+              <Technology name='Dart' />
+              <Technology name='Flutter' />
+            </div>
+          </div>
+        </div>
+        <div className='flex w-full items-center justify-center'>
+          <div className='border-2 group relative w-full max-w-md border-accents-2'>
+            <Image
+              src='/assets/media/jpg/profile.JPG'
+              width={343}
+              height={343}
+              className='h-auto w-full object-cover grayscale group-hover:grayscale-0'
+              alt='profile'
+            />
+            <div className='bg-accents-2-opaque absolute inset-0 z-10 group-hover:opacity-0' />
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from '@portfolio/components/sections//About';
 import { Banner } from '@portfolio/components/sections/Banner';
 
 export default function Home() {
   return (
     <main className=''>
       <Banner />
+      <About />
     </main>
   );
 }
