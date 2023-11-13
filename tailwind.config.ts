5b87582203946106bb2296bebbf25d814a71685f
Yuri Souza <yurdesou@gmail.com>
2023-11-12 21:34:52 -0300

    Add font sizes, colors, and border width to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,41 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        xs: ['0.625rem', '150%'], // 10px
+        sm: ['0.875rem', '150%'], // 14px
+        base: ['1rem', '150%'], // 16px
+        lg: ['1.125rem', '150%'], // 18px
+        'xl-a': ['1.25rem', '150%'], // 20px
+        'xl-b': ['1.25rem', '140%'], // 20px
+        '2xl': ['1.5rem', '140%'], // 24px
+        '3xl': ['2rem', '130%'], // 32px
+        '4xl': ['2.5rem', '120%'], // 40px
+        '5xl': ['3rem', '120%'], // 48px
+        '6xl': ['3.5rem', '120%'], // 56px
       },
+      colors: {
+        background: '#000',
+        foreground: '#fff',
+
+        'accents-1': '#111',
+        'accents-2': '#333',
+        'accents-3': '#444',
+        'accents-4': '#666',
+        'accents-5': '#888',
+        'accents-6': '#999',
+        'accents-7': '#eaeaea',
+        'accents-8': '#fafafa',
+
+        'text-1': '#A1A1A1',
+        'text-2': '#EDEDED',
+        'text-3': '#A1A1A14D',
+      },
+    },
+    borderWidth: {
+      DEFAULT: '1.5px',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
