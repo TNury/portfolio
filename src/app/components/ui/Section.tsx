95a545fcbfbf63ac063518c56d85609fc89ab290
Yuri Souza <yurdesou@gmail.com>
2023-11-15 11:16:51 -0300

    add max width to Section component

--- src/app/components/ui/Section.tsx
+++ src/app/components/ui/Section.tsx
@@ -3,5 +3,10 @@ import { cn } from '@portfolio/lib/lib';
 type SectionProps = React.HTMLAttributes<HTMLDivElement>;
 
 export const Section: React.FC<SectionProps> = ({ children, className }) => {
-  return <section className={cn('px-4 py-28 sm:px-16', className)}>{children}</section>;
+  return (
+    <section
+      className={cn('w-full max-w-screen-lg px-4 py-28 sm:px-16', className)}>
+      {children}
+    </section>
+  );
 };
