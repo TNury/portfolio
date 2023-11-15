ab424688ae2e86bc648192b4ed3849741a7cff11
Yuri Souza <yurdesou@gmail.com>
2023-11-14 21:38:07 -0300

    Update About section with tablet design

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -6,27 +6,27 @@ import { Technology } from '@portfolio/components/ui/Technology';
 export const About = () => {
   return (
     <Section className='flex flex-col gap-20'>
-      <h2 className='text-4xl font-bold text-text-2'>Sobre mim.</h2>
+      <h2 className='text-4xl font-bold text-text-2 sm:text-5xl'>Sobre mim.</h2>
       <div className='flex flex-col gap-16'>
         <div className='flex flex-col gap-8'>
           <div className='flex flex-col gap-4'>
-            <p className='text-lg text-text-1'>
+            <p className='text-lg text-text-1 sm:text-xl'>
               Lorem ipsum dolor sit amet consectetur. Vulputate augue at
               ultrices mauris convallis interdum tincidunt. Sed eu malesuada
               pellentesque ut.
             </p>
-            <p className='text-lg text-text-1'>
+            <p className='text-lg text-text-1 sm:text-xl'>
               Lorem ipsum dolor sit amet consectetur. Vulputate augue at
               ultrices mauris convallis interdum tincidunt. Sed eu malesuada
               pellentesque ut. Ac vestibulum eu enim mi quis sagittis egestas.
             </p>
-            <p className='text-lg text-text-1'>
+            <p className='text-lg text-text-1 sm:text-xl'>
               Gravida quis arcu metus lacus tristique amet augue.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3 className='text-xl-b font-bold text-text-2'>Web</h3>
-            <div className='flex flex-col gap-1'>
+            <h3 className='text-lg sm:text-xl-header font-bold text-text-2'>Web</h3>
+            <div className='flex flex-col gap-1 sm:grid sm:grid-cols-2'>
               <Technology name='JavaScript (ES6+)' />
               <Technology name='TypeScript' />
               <Technology name='React' />
@@ -36,7 +36,7 @@ export const About = () => {
             </div>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3 className='text-xl-b font-bold text-text-2'>Mobile</h3>
+            <h3 className='text-lg sm:text-xl-header font-bold text-text-2'>Mobile</h3>
             <div className='flex flex-col gap-1'>
               <Technology name='Dart' />
               <Technology name='Flutter' />
@@ -44,7 +44,7 @@ export const About = () => {
           </div>
         </div>
         <div className='flex w-full items-center justify-center'>
-          <div className='border-2 group relative w-full max-w-md border-accents-2'>
+          <div className='border-2 group relative w-full max-w-sm border-accents-2'>
             <Image
               src='/assets/media/jpg/profile.JPG'
               width={343}
