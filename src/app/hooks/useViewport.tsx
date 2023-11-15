1402cf70c6cd53bc5081e08aedd54f10e8d581ec
Yuri Souza <yurdesou@gmail.com>
2023-11-14 23:05:10 -0300

    Create useViewport hook

---
+++ src/app/hooks/useViewport.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export const useViewport = () => {
+  const [viewport, setViewport] = useState('');
+
+  useEffect(() => {
+    const breakpoints = {
+      xs: 0,
+      sm: 768,
+      md: 1024,
+      lg: 1440,
+      xl: 1920,
+    };
+
+    const updateViewport = () => {
+      const width = window.innerWidth;
+      const keys = Object.keys(breakpoints);
+      for (let i = keys.length - 1; i >= 0; i--) {
+        if (width >= breakpoints[keys[i]]) {
+          setViewport(keys[i]);
+          break;
+        }
+      }
+    };
+
+    window.addEventListener('resize', updateViewport);
+    updateViewport();
+
+    return () => window.removeEventListener('resize', updateViewport);
+  }, []);
+
+  return viewport;
+};
