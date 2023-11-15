772a62112fef104c72603b146b53e74f4e166f7c
Yuri Souza <yurdesou@gmail.com>
2023-11-15 11:17:07 -0300

    Implement max width on ContactBar component

--- src/app/components/ui/ContactBar.tsx
+++ src/app/components/ui/ContactBar.tsx
@@ -5,39 +5,47 @@ import { PhoneIcon } from '@portfolio/icons/PhoneIcon';
 
 export const ContactBar = () => {
   return (
-    <div className='fixed bottom-0 right-0 z-10 hidden w-16 flex-col items-center gap-8 md:flex'>
-      <a
-        href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
-        target='_blank'
-        className='group relative flex items-center'>
-        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
-          Yuri Souza
-        </span>
-        <LinkedInIcon className='group-hover:text-text-2' />
-      </a>
-      <a
-        href='https://github.com/TNury/'
-        target='_blank'
-        className='group relative flex items-center'>
-        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
-          TNury
-        </span>
-        <GithubIcon className='group-hover:text-text-2' />
-      </a>
-      <a href='tel:+5583981264559' className='group relative flex items-center'>
-        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
-          +55 83 98126-4559
-        </span>
-        <PhoneIcon className='group-hover:text-text-2' />
-      </a>
-      <a href='mailto:yurdesou@gmail.com' className='group relative flex items-center'>
-        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
-          yurdesou@gmail.com
-        </span>
-        <EmailIcon className='group-hover:text-text-2' />
-      </a>
-      <div className='h-44'>
-        <div className='h-full w-[1.5px] bg-accents-2' />
+    <div className='fixed bottom-0 left-0 right-0 z-10 hidden flex-col items-center gap-8 md:flex pointer-events-none'>
+      <div className='w-full flex justify-end max-w-screen-lg'>
+        <div className='flex w-16 flex-col items-center gap-8 pointer-events-auto'>
+          <a
+            href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
+            target='_blank'
+            className='group relative flex items-center'>
+            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+              Yuri Souza
+            </span>
+            <LinkedInIcon className='group-hover:text-text-2' />
+          </a>
+          <a
+            href='https://github.com/TNury/'
+            target='_blank'
+            className='group relative flex items-center'>
+            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+              TNury
+            </span>
+            <GithubIcon className='group-hover:text-text-2' />
+          </a>
+          <a
+            href='tel:+5583981264559'
+            className='group relative flex items-center'>
+            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+              +55 83 98126-4559
+            </span>
+            <PhoneIcon className='group-hover:text-text-2' />
+          </a>
+          <a
+            href='mailto:yurdesou@gmail.com'
+            className='group relative flex items-center'>
+            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+              yurdesou@gmail.com
+            </span>
+            <EmailIcon className='group-hover:text-text-2' />
+          </a>
+          <div className='h-44'>
+            <div className='h-full w-[1.5px] bg-accents-2' />
+          </div>
+        </div>
       </div>
     </div>
   );
