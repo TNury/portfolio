390b015ae95c9cd97653cb03580bc5f66e44459d
Yuri Souza <yurdesou@gmail.com>
2023-11-14 23:44:12 -0300

    Update ContactBar component with hover effect

--- src/app/components/ui/ContactBar.tsx
+++ src/app/components/ui/ContactBar.tsx
@@ -5,20 +5,36 @@ import { PhoneIcon } from '@portfolio/icons/PhoneIcon';
 
 export const ContactBar = () => {
   return (
-    <div className='hidden fixed bottom-0 right-0 w-16 flex-col items-center gap-8 md:flex'>
+    <div className='fixed bottom-0 right-0 z-10 hidden w-16 flex-col items-center gap-8 md:flex'>
       <a
         href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
-        target='_blank'>
-        <LinkedInIcon className='hover:text-text-2' />
+        target='_blank'
+        className='group relative flex items-center'>
+        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
+          Yuri Souza
+        </span>
+        <LinkedInIcon className='group-hover:text-text-2' />
       </a>
-      <a href='https://github.com/TNury/' target='_blank'>
-        <GithubIcon className='hover:text-text-2' />
+      <a
+        href='https://github.com/TNury/'
+        target='_blank'
+        className='group relative flex items-center'>
+        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
+          TNury
+        </span>
+        <GithubIcon className='group-hover:text-text-2' />
       </a>
-      <a href='tel:+5583981264559'>
-        <PhoneIcon className='hover:text-text-2' />
+      <a href='tel:+5583981264559' className='group relative flex items-center'>
+        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
+          +55 83 98126-4559
+        </span>
+        <PhoneIcon className='group-hover:text-text-2' />
       </a>
-      <a href='mailto:example@example.com'>
-        <EmailIcon className='hover:text-text-2' />
+      <a href='mailto:yurdesou@gmail.com' className='group relative flex items-center'>
+        <span className='absolute leading-none -left-80 hidden w-80 pr-4 text-right text-lg text-text-2 group-hover:block'>
+          yurdesou@gmail.com
+        </span>
+        <EmailIcon className='group-hover:text-text-2' />
       </a>
       <div className='h-44'>
         <div className='h-full w-[1.5px] bg-accents-2' />
