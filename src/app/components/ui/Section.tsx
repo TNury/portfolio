14e17116b23d4165f00f906daa193586e04f5afb
Yuri Souza <yurdesou@gmail.com>
2023-11-14 21:25:57 -0300

    Add sm padding to Section component

--- src/app/components/ui/Section.tsx
+++ src/app/components/ui/Section.tsx
@@ -3,5 +3,5 @@ import { cn } from '@portfolio/lib/lib';
 type SectionProps = React.HTMLAttributes<HTMLDivElement>;
 
 export const Section: React.FC<SectionProps> = ({ children, className }) => {
-  return <section className={cn('px-4 py-28', className)}>{children}</section>;
+  return <section className={cn('px-4 py-28 sm:px-16', className)}>{children}</section>;
 };
