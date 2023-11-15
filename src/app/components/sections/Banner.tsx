ecd9471e532ad49d19eb1a13747202d87965119f
Yuri Souza <yurdesou@gmail.com>
2023-11-14 22:52:32 -0300

    Implement laptop design onto Banner component

--- src/app/components/sections/Banner.tsx
+++ src/app/components/sections/Banner.tsx
@@ -5,11 +5,13 @@ import { Section } from '@portfolio/components/ui/Section';
 
 export const Banner = () => {
   return (
-    <Section className='relative flex min-h-screen items-center justify-center overflow-hidden '>
+    <Section className='relative flex min-h-screen items-center'>
       <div className='flex flex-col gap-8'>
         <p className='text-lg text-text-1 sm:text-xl'>Olá, meu nome é</p>
-        <h1 className='text-5xl font-bold text-text-2 sm:text-6xl'>Yuri Souza.</h1>
-        <p className='text-lg text-text-1 sm:text-xl sm:max-w-xl'>
+        <h1 className='text-5xl font-bold text-text-2 sm:text-6xl'>
+          Yuri Souza.
+        </h1>
+        <p className='text-lg text-text-1 sm:max-w-xl sm:text-xl'>
           Sou um Engenheiro de Software apaixonado por criar experiências
           digitais envolventes e funcionais.
         </p>
@@ -18,8 +20,14 @@ export const Banner = () => {
           <Button variant='secondary'>Mais sobre mim.</Button>
         </div>
       </div>
-      <div className='absolute left-0 -z-10 h-[90vh] w-[90vh] animate-spin-slow sm:left-[15vh]'>
-        <Image alt='react_icon' src='assets/media/svg/react_icon.svg' fill />
+      <div className='absolute left-0 -z-10 h-[90vh] w-[90vh] animate-spin-slow sm:left-[15vh] md:-right-[30vh] md:left-auto md:top-0 md:h-[175vh] md:w-[175vh]'>
+        <Image
+          alt='react_icon'
+          src='assets/media/svg/react_icon.svg'
+          width={1024}
+          height={1024}
+          className='h-full w-full'
+        />
       </div>
     </Section>
   );
