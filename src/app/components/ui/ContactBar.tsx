ba1bca43f056033ebe263bebfa4fd9c7467d914a
Yuri Souza <yurdesou@gmail.com>
2023-11-15 13:26:46 -0300

    Add dynamic contact details display

--- src/app/components/ui/ContactBar.tsx
+++ src/app/components/ui/ContactBar.tsx
@@ -1,48 +1,96 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { EmailIcon } from '@portfolio/icons/EmailIcon';
 import { GithubIcon } from '@portfolio/icons/GithubIcon';
 import { LinkedInIcon } from '@portfolio/icons/LinkedInIcon';
 import { PhoneIcon } from '@portfolio/icons/PhoneIcon';
 
+import { useViewport } from '@portfolio/hooks/useViewport';
+
 export const ContactBar = () => {
+  const viewport = useViewport();
+  const [openContactDetails, setOpenContactDetails] = useState(false);
+
+  useEffect(() => {
+    if (viewport !== 'xs' && viewport !== 'sm') {
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        const documentHeight = document.body.scrollHeight;
+
+        if (scrollPosition >= documentHeight - 300) {
+          setOpenContactDetails(true);
+        } else {
+          setOpenContactDetails(false);
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
   return (
-    <div className='fixed bottom-0 left-0 right-0 z-10 hidden flex-col items-center gap-8 md:flex pointer-events-none'>
-      <div className='w-full flex justify-end max-w-screen-lg'>
-        <div className='flex w-16 flex-col items-center gap-8 pointer-events-auto'>
+    <div className='pointer-events-none fixed bottom-0 left-0 right-0 z-10 hidden flex-col items-center gap-8 md:flex'>
+      <div className='flex w-full max-w-screen-lg justify-end'>
+        <div className='pointer-events-auto flex w-16 flex-col items-center gap-8'>
           <a
             href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
             target='_blank'
             className='group relative flex items-center'>
-            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+            <span
+              data-force-show={openContactDetails}
+              className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block md:data-[force-show="true"]:block'>
               Yuri Souza
             </span>
-            <LinkedInIcon className='group-hover:text-text-2' />
+            <LinkedInIcon
+              data-force-show={openContactDetails}
+              className='group-hover:text-text-2 md:data-[force-show="true"]:text-text-2'
+            />
           </a>
           <a
             href='https://github.com/TNury/'
             target='_blank'
             className='group relative flex items-center'>
-            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+            <span
+              data-force-show={openContactDetails}
+              className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block md:data-[force-show="true"]:block'>
               TNury
             </span>
-            <GithubIcon className='group-hover:text-text-2' />
+            <GithubIcon
+              data-force-show={openContactDetails}
+              className='group-hover:text-text-2 md:data-[force-show="true"]:text-text-2'
+            />
           </a>
           <a
             href='tel:+5583981264559'
             className='group relative flex items-center'>
-            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+            <span
+              data-force-show={openContactDetails}
+              className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block md:data-[force-show="true"]:block'>
               +55 83 98126-4559
             </span>
-            <PhoneIcon className='group-hover:text-text-2' />
+            <PhoneIcon
+              data-force-show={openContactDetails}
+              className='group-hover:text-text-2 md:data-[force-show="true"]:text-text-2'
+            />
           </a>
           <a
             href='mailto:yurdesou@gmail.com'
             className='group relative flex items-center'>
-            <span className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block'>
+            <span
+              data-force-show={openContactDetails}
+              className='absolute -left-80 hidden w-80 pr-4 text-right text-lg leading-none text-text-2 group-hover:block md:data-[force-show="true"]:block'>
               yurdesou@gmail.com
             </span>
-            <EmailIcon className='group-hover:text-text-2' />
+            <EmailIcon
+              data-force-show={openContactDetails}
+              className='group-hover:text-text-2 md:data-[force-show="true"]:text-text-2'
+            />
           </a>
-          <div className='h-44'>
+          <div className='h-36'>
             <div className='h-full w-[1.5px] bg-accents-2' />
           </div>
         </div>
