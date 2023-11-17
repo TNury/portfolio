9229bfbe79d1b92946e92ce2d9d157050d6b61de
Yuri Souza <yurdesou@gmail.com>
2023-11-17 19:02:32 -0300

    Refactor Button component to support different sizes

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -5,16 +5,21 @@ import { cn } from '@portfolio/lib/lib';
 import { type VariantProps, cva } from 'class-variance-authority';
 
 const buttonVariants = cva(
-  'w-fit border border-accents-2 px-6 py-3 text-lg hover:bg-accents-2 sm:text-xl',
+  'w-fit border border-accents-2 px-6 py-3 hover:bg-accents-2',
   {
     variants: {
       variant: {
         primary: 'border-accents-2 bg-accents-1',
         secondary: 'border-accents-2 bg-black',
       },
+      size: {
+        default: 'px-6 py-3',
+        sm: 'px-5 py-2',
+      },
     },
     defaultVariants: {
       variant: 'primary',
+      size: 'default',
     },
   }
 );
@@ -24,11 +29,11 @@ interface ButtonProps
     VariantProps<typeof buttonVariants> {}
 
 export const Button = (props: ButtonProps) => {
-  const { children, className, variant, ...restOfProps } = props;
+  const { children, className, variant, size, ...restOfProps } = props;
 
   return (
     <button
-      className={cn(buttonVariants({ variant, className }))}
+      className={cn(buttonVariants({ variant, size, className }))}
       {...restOfProps}>
       {children}
     </button>
