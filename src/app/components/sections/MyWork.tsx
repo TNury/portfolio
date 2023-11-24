af1f405e93e38406f3afc6d3aa3e9ef31b9445d6
Yuri Souza <yurdesou@gmail.com>
2023-11-23 22:51:49 -0300

    Update MyWork component to include description

--- src/app/components/sections/MyWork.tsx
+++ src/app/components/sections/MyWork.tsx
@@ -7,18 +7,25 @@ import { staticProjects } from 'src/app/static-data/staticData';
 
 export const MyWork = () => {
   return (
-    <Section id="trabalho" className='flex flex-col gap-20'>
+    <Section id='trabalho' className='flex flex-col gap-20'>
       <h2 className='text-center text-4xl font-bold text-text-2 sm:text-5xl'>
         Meu trabalho.
       </h2>
-      <div className='flex flex-col gap-28'>
-        {staticProjects.map((project, index) => (
-          <ProjectBox
-            key={index}
-            projectProps={project}
-            parity={isOddOrEven(index)}
-          />
-        ))}
+      <div className='flex flex-col gap-16 md:items-center'>
+        <p className='text-lg text-text-1 sm:max-w-xl sm:text-xl md:text-center'>
+          Na minha trajetória como desenvolvedor, trabalhei no desenvolvimento
+          de diversos projetos. Listados abaixo estão alguns dos meus trabalhos
+          mais notáveis.
+        </p>
+        <div className='flex flex-col gap-28 w-full'>
+          {staticProjects.map((project, index) => (
+            <ProjectBox
+              key={index}
+              projectProps={project}
+              parity={isOddOrEven(index)}
+            />
+          ))}
+        </div>
       </div>
     </Section>
   );
