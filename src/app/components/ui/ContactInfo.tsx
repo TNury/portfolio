c4229c05e30788c02be995bf69d2e4c5afd15404
Yuri Souza <yurdesou@gmail.com>
2023-11-24 14:43:17 -0300

    Add ContactInfo component

---
+++ src/app/components/ui/ContactInfo.tsx
@@ -0,0 +1,52 @@
+import { EmailIcon } from '@portfolio/icons/EmailIcon';
+import { GithubIcon } from '@portfolio/icons/GithubIcon';
+import { LinkedInIcon } from '@portfolio/icons/LinkedInIcon';
+import { WhatsappIcon } from '@portfolio/icons/WhatsappIcon';
+
+import { cn } from '@portfolio/lib/lib';
+
+type ContactInfoProps = React.HTMLAttributes<HTMLDivElement>;
+
+export const ContactInfo: React.FC<ContactInfoProps> = ({
+  className,
+  ...restOfProps
+}) => {
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-col items-center justify-center gap-8 md:hidden',
+        className
+      )}
+      {...restOfProps}>
+      <div className='flex flex-col items-center gap-2'>
+        <h3 className='text-lg font-bold text-text-2 sm:text-xl-header'>
+          Whatsapp
+        </h3>
+        <a href='tel:+5583981264559' className='flex items-center gap-2'>
+          <WhatsappIcon className='hover:text-text-2' />
+          <span className='text-base text-text-1 sm:text-lg'>
+            +55 (83) 98126-4559
+          </span>
+        </a>
+      </div>
+      <div className='flex flex-col items-center gap-2'>
+        <h3 className='text-lg font-bold text-text-2 sm:text-xl-header'>
+          Redes Sociais
+        </h3>
+        <div className='flex gap-8'>
+          <a
+            href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
+            target='_blank'>
+            <LinkedInIcon className='hover:text-text-2' />
+          </a>
+          <a href='https://github.com/TNury/' target='_blank'>
+            <GithubIcon className='hover:text-text-2' />
+          </a>
+          <a href='mailto:example@example.com'>
+            <EmailIcon className='hover:text-text-2' />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
