4e503056037b98d92fd77c0f1d639d5b3c94d6c3
Yuri Souza <yurdesou@gmail.com>
2023-11-27 13:59:31 -0300

    Add Clarity analytics script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,15 @@ export default function RootLayout({
     <html lang='pt-br'>
       <head>
         <link rel='icon' href='/assets/media/ico/favicon.ico' sizes='512x512' />
+        <script type='text/javascript'>
+          {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jx59j6sdh8");`
+          }
+        </script>
       </head>
       <body className={cn(GeistMono.className, 'bg-background')}>
         <Header links={staticHeaderData} />
